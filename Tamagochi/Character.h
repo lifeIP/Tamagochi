070ec737a511711b53c9c37b9e12b2155502bc7f
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-08-16 23:08:10 +0700

    Добавьте файлы проекта.

---
+++ Tamagochi/Character.h
@@ -0,0 +1,16 @@
+#pragma once
+class Character
+{
+	short level_health; 
+	short level_hunger;
+	short level_fatigue;
+public:
+	short get_level_health()const;
+	short get_level_hunger()const;
+	short get_level_fatigue()const;
+	void feed();
+	void play();
+	void cradle();
+	
+};
+
